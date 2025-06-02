d01038b22cdf03f2b912024f689ce313be8f56a1
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-02 18:33:56 +0530

    5: restructured the files

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link';
+import classes from './page.module.css'
+
 export default function MealsPage() {
     return (
-        <div>
-        <h1>Meals Page</h1>
-        <p>Welcome to the Meals page!</p>
-        </div>
+        <>
+        <header className={classes.header}>
+            <h1>
+            Delicious Meals, created {''}
+            <span className={classes.highlight}>by you</span>
+            </h1>
+        </header>
+        <p>Choose your fav recipe. It's easy and very fun !!!</p>
+        <p className={classes.cta}>
+            <Link href='/meals/share'>
+            Share Your fav Meals!
+            </Link>
+        </p>
+        <main className={classes.main}></main>
+        </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 import classes from './page.module.css';
-import ImageSlideshow from '@/components/main-header/images/images-slideshow';
+import ImageSlideshow from '@/components/images/images-slideshow';
 
 export default function Home() {
   return (
