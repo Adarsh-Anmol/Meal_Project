a0148bc298d21fdc4014ff024261381d0fc035a9
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-03 17:27:59 +0530

    7: added the loading file

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,10 +1,14 @@
 import sql from 'better-sqlite3';
-import { MealItemProps } from "@/components/meals/meal-item";
+import { MealItemProps } from "@/components/meals/meal-item"; 
+ //the type of the meal item
 
 
 const db = sql('meals.db');
 
 export async function getMeals(): Promise<MealItemProps[]> {
-    await new Promise ((resolve) => setTimeout(resolve, 2000)); // Simulating a delay for async operation
-    return db.prepare('SELECT * FROM meals').all() as MealItemProps[]; //all for fetching all rows of  data; get for single row of data;
+    await new Promise ((resolve) => setTimeout(resolve, 2000));
+     // Simulating a delay for async operation
+    return db.prepare('SELECT * FROM meals').all() as MealItemProps[]; 
+    //all for fetching all rows of data;
+    //  get for single row of data; run for inserting data
 }
