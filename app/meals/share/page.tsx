833057abfa3a78a2678d83589b3ec0595bf07863
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-04 18:45:00 +0530

    14: seperated the server component from meals sharing form

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,24 +1,8 @@
-import { title } from 'process';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-
-  async function shareMeal(formData: FormData) {
-    'use server'; // This function will be executed on the server side. This is called a server action.
-
-    const meal ={
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    }
-
-    console.log('Meal shared:', meal);
-  }
-
 
   return (
     <>
---
+++ lib/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+async function shareMeal(formData: FormData) {
+    'use server'; // This function will be executed on the server side. This is called a server action.
+
+    const meal ={
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email'),
+    }
+
+    console.log('Meal shared:', meal);
+  }
